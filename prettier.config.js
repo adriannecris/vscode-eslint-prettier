e70badfa5b8a4926938b6f8ba851a2313fa909b2
keyda <35050635+adriannecris@users.noreply.github.com>
2023-06-06 11:54:44 +0800

    Create prettier.config.js
    Added personal prettier preference

---
+++ prettier.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  printWidth: 140,
+  semi: false,
+  singleQuote: false,
+  trailingComma: "es5",
+  singleAttributePerLine: true,
+//   plugins: [require("prettier-plugin-tailwindcss")],
+}
